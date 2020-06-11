f83ab055484aed4d47493a0ffdbcb84290edddaa
[Mario Nuñez] <[mario.nunezf@gmail.com]>
2020-06-11 09:29:42 -0400

    Version Final

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-// Requires
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
 
-//Heroku
-const port = process.env.PORT || 3000;
-//Inicializar variables
-var app = express();
-
-//MiddleWare CORS
+const app = express();
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -16,35 +9,10 @@ app.use(function(req, res, next) {
     next();
 });
 
-//Body Parser
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.static(__dirname + '/dist/storeapp'));
 
-//importar Rutas
-var appRoutes = require('./routes/app');
-var usuarioRoutes = require('./routes/usuario');
-var loginRoutes = require('./routes/login');
-var productoRoutes = require('./routes/producto');
-var categoriaRoutes = require('./routes/categoria');
-var busquedaRoutes = require('./routes/busqueda');
-
-//Conexion BD atlas con usuario userReadWrite 
-const uri = "mongodb+srv://userReadWrite:af29101988@hospitadb-uewzz.mongodb.net/StoreApp?retryWrites=true&w=majority";
-
-mongoose.connect(uri, (err, res) => {
-    if (err) throw err;
-    console.log('Base de Datos Mongo Atlas: \x1b[32m%s\x1b[0m', 'Online');
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/storeapp/index.html'));
+    console.log(`Express Server puerto 80: \x1b[32m%s\x1b[0m`, 'Online');
 });
-
-//Rutas la principal va al final.
-app.use('/usuario', usuarioRoutes);
-app.use('/login', loginRoutes);
-app.use('/busqueda', busquedaRoutes);
-app.use('/producto', productoRoutes);
-app.use('/categoria', categoriaRoutes);
-app.use('/', appRoutes);
-
-// Escuchar peticiones
-app.listen(port, () => {
-    console.log(`Express Server puerto ${port}: \x1b[32m%s\x1b[0m`, 'Online');
-})
+app.listen(5000);
